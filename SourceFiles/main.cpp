a75d8e67d1e093019e6940e45c646ceb3e394e3c
davborje <davborje@student.chalmers.se>
2021-03-26 11:28:03 +0100

    primechecker, part of assignment 6

---
+++ SourceFiles/main.cpp
@@ -0,0 +1,15 @@
+#include "../HeaderFiles/PrimeChecker.hpp"
+#include <iostream>
+
+main (int argc, char **argv)
+{
+  if (argc == 2)
+    {
+
+      int number = std::stoi (argv[1]);
+      PrimeChecker pc;
+      std::cout << "last name, first name; " << number
+                << " is a prime number? " << pc.isPrime (number) << std::endl;
+    }
+  return 0;
+}
