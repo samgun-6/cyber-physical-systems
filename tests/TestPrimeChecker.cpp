4d316168615da22a6bdff0d1c8e50c3f8eee97ae
davborje <davborje@student.chalmers.se>
2021-04-01 21:54:20 +0200

    Added more tests, included failed ones

--- tests/TestPrimeChecker.cpp
+++ tests/TestPrimeChecker.cpp
@@ -7,3 +7,22 @@ TEST_CASE("Test PrimeChecker 1.") {
     PrimeChecker pc;
     REQUIRE(pc.isPrime(5));
 }
+
+TEST_CASE("Test PrimeChecker 2.") {
+    PrimeChecker pc;
+    REQUIRE(pc.isPrime(1));
+}
+
+TEST_CASE("Test PrimeChecker 3.") {
+    PrimeChecker pc;
+    REQUIRE(pc.isPrime(4));
+}
+
+TEST_CASE("Test PrimeChecker 4.") {
+    PrimeChecker pc;
+    REQUIRE(pc.isPrime(6));
+}
+
+
+
+
