17b6a277f319f19929acf95083b3105add99cc55
erichal <gushalerb@student.gu.se>
2021-05-04 15:33:12 +0200

    add performance calculations for steering angle

--- src/SteeringCalculation.py
+++ src/SteeringCalculation.py
@@ -4,66 +4,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
-def testGraph(ourFile, originalFile):
-    originalFile['Lower'] = originalFile['groundSteering'] / 2.0
-    originalFile['Upper'] = originalFile['groundSteering'] * 1.5
-
-    #needs the length of the number of rows in the file
-    x = np.arange(1,816,1)
-
-    y1 = originalFile['groundSteering'] + 1
-    y2 = originalFile['Lower'] +1
-    y3 = originalFile['Upper'] +1
-
-    plt.plot(x, y1,'b',  label = "Actual")
-    plt.plot(x, y2,  label = "Lower threshold")
-    plt.plot(x, y3,  label = "Upper threshold")
-
-    plt.fill_between(x, y1, y2, where=y1 < y2, color='#539ecd')
-    plt.fill_between(x, y1, y2, where=y1 > y2, color='#539ecd')
-
-    plt.fill_between(x, y1, y2, where=y1 > y3, color='#539ecd')
-    plt.fill_between(x, y1, y2, where=y1 < y3, color='#539ecd')
-
-    plt.grid()
-    plt.legend()
-    plt.title('Showing areas of deviation', fontsize=10)
-    plt.show()
-
-
-
 def comparisonSteering(mergedFile):
 
-    mergedFile['InRange'] = (mergedFile['Lower'] < mergedFile['Esteeringangle']) & (mergedFile['Upper'] > mergedFile['Esteeringangle'])
-    #mergedFile['InRange'] = (mergedFile.Atimestamp == mergedFile.Etimestamp) & (mergedFile.Esteeringangle == mergedFile.Asteeringangle)
+    # Adding a new column with Boolean if the Steering Agnle is within range
+    mergedFile['InRange'] = (mergedFile['Lower'] < mergedFile['Asteeringangle']) & (mergedFile['Upper'] > mergedFile['Asteeringangle'])
+
+    # printing the result
     print("This is the equality")
-    print(mergedFile.InRange)
+    print(mergedFile.InRange.value_counts())
 
 
 def plotLineGraphComp(ourFile, originalFile):
+
+    # adding the data to plot
     plt.plot(originalFile['groundSteering'], label = "Expected")
     plt.plot(ourFile['groundSteering'], label = "Actual")
 
-    plt.ylabel('Steering Angel 0.1 = 10 degrees')
+    # plotting 
+    plt.ylabel('Steering Angel')
     plt.xlabel('N timestamps')
     plt.title('Comparison of steering angel')
-    plt.legend()
-
-    plt.show()
-
-
-
-def plotLineGraphULT(ourFile, originalFile):
-
-    plt.plot(originalFile['Lower'], label = "lower threshold")
-    plt.plot(originalFile['Upper'], label = "upper threshold")
-    plt.plot((ourFile['groundSteering']), label = "Actual")
-    #plt.plot(originalFile['groundSteering'], label = "Expected")
-
-    plt.ylabel('Steering Angel 0.1 = 10 degrees')
-    plt.xlabel('N timestamps')
-    plt.title(' Steering angel with upper/lower thresholds')
     plt.legend()
 
     plt.show()
@@ -75,8 +35,14 @@ def sliceAndCalculateTimestamp(df):
     df['sampleTimeStamp.seconds'] = df['sampleTimeStamp.seconds'].astype(str)
     df['sampleTimeStamp.microseconds'] = df['sampleTimeStamp.microseconds'].astype(str)
     df['Timestamp'] = df['sampleTimeStamp.seconds'] + df['sampleTimeStamp.microseconds']
+
+    # Adding new columns with threshold values 
     df['Lower'] = df['groundSteering'] / 2.0
     df['Upper'] = df['groundSteering'] * 1.5
+
+    # Adding threshold values for columns with value 0
+    df.loc[df['groundSteering'] == 0, 'Upper'] = 0.05
+    df.loc[df['groundSteering'] == 0, 'Lower'] = -0.05
 
     # deleting redundant columns
     del df['sent.seconds']
@@ -107,21 +73,18 @@ def sliceAndCalculateTimestampImportedFile(df):
     del df['sampleTimeStamp.microseconds']
 
     # casting the Timestamp to a float
-    df['Timestamp'] = df['Timestamp'].astype('int64')
+    df['Timestamp'] = df['Timestamp'].astype('float64')
 
     # returing the dataframe
     return df
 
 # importing csvs
 csvFile = pd.read_csv("calculated-steering-angle.csv", sep = ";")
-csvFileOriginal = pd.read_csv("1452.csv", sep = ";")
+csvFileOriginal = pd.read_csv("1456.csv", sep = ";")
 
 # Slicing and reformatting into dataframes
 csvFile = sliceAndCalculateTimestampImportedFile(csvFile)
 csvFileOriginal = sliceAndCalculateTimestamp(csvFileOriginal)
-
-# dropping n last rows to have equal number of rows to make comparisons
-csvFile.drop(csvFile.tail(7).index,inplace=True)
 
 # Merging the dataframes and renaming the columns
 merged = pd.concat([csvFile, csvFileOriginal], axis=1)
@@ -130,27 +93,5 @@ merged.columns = ['Asteeringangle', 'Atimestamp', 'Esteeringangle', 'Etimestamp'
 # Calculating the equality of steering angle on the two files based on index (row by row), NOT A TIMESTAMP COMPARISON
 comparisonSteering(merged)
 
-# printing
-#print("Merged----------------------------------------------------------------------------------")
-#print(merged.info)
-#print(merged.head(10))
-#print(merged.columns)
-#
-#print("Actual----------------------------------------------------------------------------------")
-#print(csvFile.info)
-#print(csvFile.dtypes)
-#print(csvFile.head(20))
-#
-#print("Expected-----------------------------------------------------------------------------------")
-#print(csvFileOriginal.info)
-#print(csvFileOriginal.dtypes)
-#print(csvFileOriginal.head(20))
-
-
 # plotting
 plotLineGraphComp(csvFile, csvFileOriginal)
-plotLineGraphULT(csvFile, csvFileOriginal)
-
-#testGraph(merged)
-
-
